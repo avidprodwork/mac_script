dd5abc33323640f78ec2202b28af3e7bb6be6e99
pagasyan.a <avidprodwork@gmail.com>
2026-03-30 17:18:16 +0300

    4

--- setup.sh
+++ setup.sh
@@ -24,14 +24,14 @@ step() {
     fi
 }
 
-# Установка Homebrew
-step "Установка Homebrew" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+# Установка Homebrew (автоматический Enter)
+step "Установка Homebrew" 'printf "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 
 # Настройка окружения brew
 step "Настройка окружения brew" 'echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> /Users/$USER/.zprofile && eval "$(/opt/homebrew/bin/brew shellenv)"'
 
-# Установка Dozer (через tap, если доступен)
-step "Установка Dozer" 'brew tap mortennn/dozer && brew install --cask dozer'
+# Установка Dozer вручную
+step "Установка Dozer" 'curl -L -o ~/Downloads/Dozer.dmg https://github.com/Mortennn/Dozer/releases/download/v4.0.0/Dozer.4.0.0.dmg && hdiutil attach ~/Downloads/Dozer.dmg && cp -r /Volumes/Dozer/Dozer.app /Applications && hdiutil detach /Volumes/Dozer && rm ~/Downloads/Dozer.dmg'
 
 # Создание директорий
 step "Создание директорий" 'mkdir -p /Applications/user ~/Desktop/Certifikates && chflags hidden /Applications/user'
@@ -48,15 +48,17 @@ step "Отключение сна" 'run_sudo pmset -a disablesleep 1'
 # Установка пакетов
 step "Установка пакетов" 'brew install blueutil cocoapods rbenv && blueutil --power 0'
 
-# Установка Ruby и гемов
-step "Установка Zeitwerk" 'run_sudo gem install zeitwerk -v 2.7.5'
-step "Установка ActiveSupport" 'run_sudo gem install activesupport -v 8.1.3'
-step "Установка DRb" 'run_sudo gem install drb -v 2.2.3'
-
 # Настройка rbenv
-step "Настройка rbenv" 'echo "export PATH=\"$HOME/.rbenv/bin:$PATH\"" >> ~/.bash_profile && echo "eval \"$(rbenv init --no-rehash)\"" >> ~/.bash_profile'
-step "Установка Ruby 4.0.2" 'run_sudo rbenv install 4.0.2 && run_sudo rbenv global 4.0.2 && source ~/.bash_profile && ruby -v'
-step "Установка CocoaPods" 'run_sudo gem install cocoapods -v 1.16.2'
+step "Настройка rbenv" 'echo "export PATH=\"$HOME/.rbenv/bin:$PATH\"" >> ~/.bash_profile && echo "eval \"$(rbenv init -)\"" >> ~/.bash_profile && source ~/.bash_profile'
+
+# Установка Ruby 4.0.2
+step "Установка Ruby 4.0.2" 'rbenv install 4.0.2 && rbenv global 4.0.2 && ruby -v'
+
+# Установка гемов (без sudo, чтобы ставились в ~/.rbenv)
+step "Установка Zeitwerk" 'gem install zeitwerk -v 2.7.5'
+step "Установка ActiveSupport" 'gem install activesupport -v 8.1.3'
+step "Установка DRb" 'gem install drb -v 2.2.3'
+step "Установка CocoaPods" 'gem install cocoapods'
 
 # Очистка истории zsh
 step "Очистка истории zsh" 'rm ~/.zsh_history && touch ~/.zsh_history'
