aea8a88b1790b73b05d44b6e30491315f079ff9b
pagasyan.a <avidprodwork@gmail.com>
2026-03-30 17:30:31 +0300

    6

--- setup.sh
+++ setup.sh
@@ -21,20 +21,29 @@ step() {
     fi
 }
 
-# Установка Homebrew (ты сам нажмёшь Enter при запуске)
+# Установка Homebrew (ты сам нажмёшь Enter)
 step "Установка Homebrew" '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
 
 # Настройка окружения brew
 step "Настройка окружения brew" 'echo "eval \"$(/opt/homebrew/bin/brew shellenv)\"" >> /Users/$USER/.zprofile && eval "$(/opt/homebrew/bin/brew shellenv)"'
 
-# Установка Dozer вручную
+# Установка Dozer
 step "Установка Dozer" 'curl -L -o ~/Downloads/Dozer.dmg https://github.com/Mortennn/Dozer/releases/download/v4.0.0/Dozer.4.0.0.dmg && hdiutil attach ~/Downloads/Dozer.dmg && cp -r /Volumes/Dozer/Dozer.app /Applications && hdiutil detach /Volumes/Dozer && rm ~/Downloads/Dozer.dmg'
+
+# Установка Keeper
+step "Установка Keeper" 'curl -L -o ~/Downloads/Keeper.dmg https://www.keepersecurity.com/desktop_electron/Darwin/KeeperSetup.dmg && hdiutil attach ~/Downloads/Keeper.dmg && cp -r /Volumes/Keeper/Keeper.app /Applications && hdiutil detach /Volumes/Keeper && rm ~/Downloads/Keeper.dmg'
+
+# Установка Cisdem AppCrypt
+step "Установка Cisdem AppCrypt" 'curl -L -o ~/Downloads/Cisdem.dmg https://download.cisdem.com/cisdem-appcrypt.dmg && hdiutil attach ~/Downloads/Cisdem.dmg && cp -r /Volumes/AppCrypt/Cisdem\ AppCrypt.app /Applications && hdiutil detach /Volumes/AppCrypt && rm ~/Downloads/Cisdem.dmg'
+
+# Установка Unity Hub
+step "Установка Unity Hub" 'curl -L -o ~/Downloads/UnityHub.dmg https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.dmg && hdiutil attach ~/Downloads/UnityHub.dmg && cp -r /Volumes/Unity\ Hub/Unity\ Hub.app /Applications && hdiutil detach /Volumes/Unity\ Hub && rm ~/Downloads/UnityHub.dmg'
 
 # Создание директорий
 step "Создание директорий" 'mkdir -p /Applications/user ~/Desktop/Certifikates && chflags hidden /Applications/user'
 
 # Перемещение приложений (с проверкой)
-step "Перемещение приложений" '[ -d "/Applications/Dozer.app" ] && mv /Applications/Dozer.app /Applications/user; [ -d "/Applications/Cisdem AppCrypt.app" ] && mv "/Applications/Cisdem AppCrypt.app" /Applications/user'
+step "Перемещение приложений" '[ -d "/Applications/Dozer.app" ] && mv -f /Applications/Dozer.app /Applications/user; [ -d "/Applications/Cisdem AppCrypt.app" ] && mv -f "/Applications/Cisdem AppCrypt.app" /Applications/user; [ -d "/Applications/Keeper.app" ] && mv -f "/Applications/Keeper.app" /Applications/user; [ -d "/Applications/Unity Hub.app" ] && mv -f "/Applications/Unity Hub.app" /Applications/user'
 
 # Создание файлов
 step "Создание файлов" 'touch ~/Desktop/Certifikates/SharedSecret.txt ~/Desktop/AppCryptSitesList.txt'
@@ -45,7 +54,7 @@ step "Отключение сна" 'run_sudo pmset -a disablesleep 1'
 # Установка пакетов
 step "Установка пакетов" 'brew install blueutil cocoapods rbenv && blueutil --power 0'
 
-# Настройка rbenv (исправлено)
+# Настройка rbenv
 step "Настройка rbenv" 'echo "export PATH=\"$HOME/.rbenv/bin:$PATH\"" >> ~/.bash_profile && echo "eval \"$(rbenv init -)\"" >> ~/.bash_profile && source ~/.bash_profile'
 
 # Установка Ruby 4.0.2
